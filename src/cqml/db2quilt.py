e32e7b6f4b1a3f21a918fc05a4a0a0eb5555fe05
drernie <ernest@nauto.com>
2022-03-03 20:47:47 -0800

    pass pypyr make test

--- src/cqml/db2quilt.py
+++ src/cqml/db2quilt.py
@@ -143,10 +143,11 @@ class Package:
     def cleanup(self, msg, meta = {"db2quilt":"v0.1"}):
         self.write_summary()
         QPKG.set_dir('/',path=self.path, meta=meta)
-        if MOCK:
-            QPKG.push(self.name, self.proj.repo, message=msg) #, force=True
-        else:
-            QPKG.push(self.name, self.proj.repo, message=msg,force=True) #,
+        print(f'MOCK={MOCK}')
+        #if MOCK:
+        #    QPKG.push(self.name, self.proj.repo, message=msg) #, force=True
+        #else:
+        QPKG.push(self.name, self.proj.repo, message=msg,force=True) #,
         #shutil.rmtree(self.path)
         self.html = f'Published <a href="{self.url}">{self.name}</a> for <b>{msg}</b>'
         return self
