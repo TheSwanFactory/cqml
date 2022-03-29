333ae1d7984243f9721544b07ecec5a4e3f421b4
drernie <ernest@nauto.com>
2022-03-28 17:01:17 -0700

    "post-regen-tests/cqml_test_db.py-0.4.0.dev4"

--- tests/cqml_test_db.py
+++ tests/cqml_test_db.py
@@ -10,7 +10,7 @@
 #!pip install cqml
 #--IMPORT-CQML--#
 !pip --no-cache-dir install git+https://github.com/TheSwanFactory/cqml.git@v04-trips
-!pip install cqml==0.4.0.dev3
+!pip install cqml==0.4.0.dev4
 
 #++IMPORT-CQML++#
 
@@ -25,9 +25,6 @@ cvm.run()
 
 # COMMAND ----------
 
-print(cvm.df.keys())
-dt = cvm.df['calc_quarters']
-dt.show()
 #dict = cvm.do_save({})
 
 # COMMAND ----------
