754cab8a6c27ffd8f9544402c00ff6f9eab661ca
drernie <ernest@nauto.com>
2022-05-12 08:29:20 -0700

    class CQML_Parser

---
+++ src/cqml/parse.py
@@ -0,0 +1,27 @@
+import os, yaml
+
+class CQML_Parser:
+    def __init__(self, root):
+        self.nodes = []
+        self.scan(root)
+
+    def scan(self, root):
+        for entry in os.scandir(root):
+            self.parse(entry, root)
+
+    def parse(self, entry, folder):
+        if entry.name.endswith(".yml"):
+            key = os.path.splitext(entry.name)[0]
+            yml = read_yaml(entry.path)
+            node = {
+                "file":entry.name,
+                "folder":folder,
+                "path": entry.path,
+                 "key": key,
+                 "fkey": f"{folder}.{key}",
+                 "yml": yml
+             }
+            self.nodes.append(node)
+        elif entry.is_dir():
+            print(entry.name)
+            self.scan(entry.name)
