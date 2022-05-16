9d1fd82324fe493f834ae77d2611001ee5c0277d
drernie <ernest@nauto.com>
2022-05-15 21:39:04 -0700

    "post-regen-tests/cqml_test_db.py-0.5.0.dev10"

--- tests/cqml_test_db.py
+++ tests/cqml_test_db.py
@@ -9,7 +9,7 @@
 !pip install --upgrade pip
 #!pip install cqml
 !pip --no-cache-dir install git+https://github.com/TheSwanFactory/cqml.git@convert45
-!pip install cqml==0.5.0.dev9
+!pip install cqml==0.5.0.dev10
 
 import cqml
 
